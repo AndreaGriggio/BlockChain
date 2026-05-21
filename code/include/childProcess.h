47da6eda1b07ca3737eb9cb3050c3549a809b658
Andrea Griggio <andrea.griggio@studenti.unitn.it>
2026-05-21 20:28:16 +0200

    Aggiornamento childProcess

--- code/include/childProcess.h
+++ code/include/childProcess.h
@@ -6,16 +6,39 @@
 #define CHILDPROCESS_H
 #include <signal.h>
 
+/**
+ *indica il ruolo del processo :
+ */
 typedef enum Ruolo {
     CLIENT = 0,
     MINER = 1,
     NODE = 2
 }Ruolo;
-typedef struct ChildProcess ChildProcess;
+typedef struct ChildProcess ChildProcess;//Non ha una struttura è definita in childprocess.c
 
-ChildProcess * childProcessCreate(pid_t pid,int id , Ruolo r);//costruttore
+/**
+ *Alloca spazio per la struttura di ChildProcess
+ *
+ */
+ChildProcess* childProcessCreate();
+/**
+ *Inizializza la struttura ChildProcess
+ *
+ *La struttura serve per tenere traccia di tutti i processi è compagna di tutti i processi generati dal processo
+ *@param child_ptr zona di memoria in cui mettere la struct
+ *@param pid pid del processo in cui viene memorizzata la struttura
+ *@param id id del processo assegnato al punto di creazione del processo
+ *@param r ruolo del processo
+ *
+ */
+int childProcessInit(ChildProcess* child_ptr,pid_t pid,int id , Ruolo r);
 
-void childProcessDestroy(ChildProcess* child);//distruttore
+
+/**
+ *
+ * @param child Puntat
+ */
+void childProcessDestroy(ChildProcess* child_ptr);
 
 const char* getCpToString(const ChildProcess* c);
 int getCpInt(const ChildProcess* c);
--- code/src/childProcess.c
+++ code/src/childProcess.c
@@ -3,3 +3,47 @@
 //
 
 #include "childProcess.h"
+#include "stdlib.h"
+#include <stdio.h>
+typedef struct ChildProcess {
+    pid_t pid;
+    Ruolo r;
+    int id;
+}ChildProcess;
+
+ChildProcess* childProcessCreate() {return malloc(sizeof(ChildProcess));}
+
+int childProcessInit(ChildProcess* child_ptr,pid_t pid,int id,Ruolo r) {
+    if (child_ptr == NULL) return 1;
+    child_ptr->pid = pid;
+    child_ptr->r = r;
+    child_ptr->id = id;
+    return 0;
+}
+void childProcessDestroy(ChildProcess *child_ptr) {
+    if (child_ptr == NULL) return;
+    free(child_ptr);
+}
+
+int getCpToString(const ChildProcess *child_ptr, char *buffer, size_t buffer_size) {
+    if (child_ptr == NULL || buffer == NULL || buffer_size == 0) {
+        return 1;
+    }
+    const char *role_str;
+
+    switch (child_ptr->r) {
+        case CLIENT:role_str = "Client";break;
+        case MINER:role_str = "Miner";break;
+        case NODE:role_str = "Node";break;
+        default:role_str = "No Role";break;
+    }
+
+    snprintf(buffer,
+             buffer_size,
+             "ChildProcess { pid: %d, id: %d, role: %s }",
+             child_ptr->pid,
+             child_ptr->id,
+             role_str);
+
+    return 0;
+}
